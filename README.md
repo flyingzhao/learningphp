7372b08db2d58d742f3abc6489ccb21714159eb8
zhao <1142749677@qq.com>
2015-07-10 14:38:37 +0800

    ch4

---
+++ README.md
@@ -0,0 +1,5 @@
+PHP and MySQL Web Development
+Ubuntu 14.04
+PHP 5.5.9
+MySQL 5.5
+Apache/2.4.7 (Ubuntu) Server at localhost Port 80
--- ch4/processfeedback.php
+++ ch4/processfeedback.php
@@ -1,15 +1,39 @@
 <?php
-	$name=$_POST['name'];
-	$email=$_POST['email'];
-	$feedback=$_POST['feedback'];
+	$name=trim($_POST['name']);
+	$email=trim($_POST['email']);
+	$feedback=trim($_POST['feedback']);
 
+	// echo $name."<br />".$email."<br />".$feedback."<br />";
 	//set up some static information
 	$toaddress="1142749677@qq.com";#feedback@example.com
 	$subject="Feedback from website";
 	$mailcontent="Customer name:".$name."\n".
-			"Customer email".$email."\n".
-			"Customer Comments".$feedback."\n";
+			"Customer email:".$email."\n".
+			"Customer Comments:".$feedback."\n";
+	// echo $mailcontent;
+	echo nl2br($mailcontent);
 	$fromaddress="From:  webserver@example.com";
+
+	printf("total amount is %d",12);
+
+	//slash
+	echo "<p>feedback before slash</p>:";
+	$feed="Your customer service told me \"you don't need any guarantee\"";
+	echo "<p>$feed</p>";
+	echo "<p>feedback after adding slash:</p>";
+	echo  "<p>".addslashes($feed)."</p>";
+	echo "<p>feedback after striping slash:</p>";
+	echo  "<p>".stripslashes($feed)."</p>";
+
+	//explode
+	$emailarray=explode('@', $email);
+	if(strtolower($emailarray[1])=="bigcustomer.com"){
+		$toaddress="Bob@example.com";
+	}
+	else{
+		$toaddress="feedback@example.com";
+	}
+	$new_email=implode('@', $emailarray);
 
 	//invoke mail() function to send mail
 	mail($toaddress,$subject,$mailcontent,$fromaddress);//使用mail()函数发送邮件，必须要有一台不需要验证的SMTP服务器。
