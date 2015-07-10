2fb09fd30e49298b7fc0c3baafca947105b00a43
zhao <1142749677@qq.com>
2015-07-10 21:29:58 +0800

    readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-<p>PHP and MySQL Web Development(Fourth Edition)</p>
+<p><strong>PHP and MySQL Web Development(Fourth Edition)</strong></p>
 Ubuntu 14.04 <br />
 PHP 5.5.9 <br />
 MySQL 5.5<br />
--- ch4/processfeedback.php
+++ ch4/processfeedback.php
@@ -5,7 +5,7 @@
 
 	// echo $name."<br />".$email."<br />".$feedback."<br />";
 	//set up some static information
-	$toaddress="1142749677@qq.com";#feedback@example.com
+	$toaddress="feedback@example.com";
 	$subject="Feedback from website";
 	$mailcontent="Customer name:".$name."\n".
 			"Customer email:".$email."\n".
@@ -13,11 +13,14 @@
 	// echo $mailcontent;
 	echo nl2br($mailcontent);
 	$fromaddress="From:  webserver@example.com";
+	//invoke mail() function to send mail
+	mail($toaddress,$subject,$mailcontent,$fromaddress);//使用mail()函数发送邮件，必须要有一台不需要验证的SMTP服务器。
 
+	//printf
 	printf("total amount is %d",12);
 
 	//slash
-	echo "<p>feedback before slash</p>:";
+	echo "<p>feedback before slash:</p>";
 	$feed="Your customer service told me \"you don't need any guarantee\"";
 	echo "<p>$feed</p>";
 	echo "<p>feedback after adding slash:</p>";
@@ -35,8 +38,46 @@
 	}
 	$new_email=implode('@', $emailarray);
 
-	//invoke mail() function to send mail
-	mail($toaddress,$subject,$mailcontent,$fromaddress);//使用mail()函数发送邮件，必须要有一台不需要验证的SMTP服务器。
+	//substr
+	$test='Your customer service is excellent';
+	echo substr($test, 5,3);
+
+	//strlen
+	if(strlen($email)<6){
+		echo 'That email address is not valid';
+		// exit;
+	}
+
+	//strstr
+	if (strstr($feedback, 'shop')) {
+		$toaddress='retail@example.com';
+	}
+	elseif (strstr($feedback, 'delivery')) {
+		$toaddress='fulfillment@example.com';
+	}
+	elseif (strstr($feedback, 'bill')) {
+		$toaddress='account@example.com';
+	}
+	echo '<p>'.strstr("helloworld", 'o').'</p>';
+	echo "<p>".strpos("helloworld", 'l').'</p>';
+	$result=strpos("helloworld", 'l');
+	if ($result===false) {
+		echo "<p>Not found</p>";
+	}
+	else{
+		echo "<p>Found at position</p>".$result;
+	}
+
+	//str_replace
+	$offcolor = array('shit' ,'fuck');
+	$feedback=array("shit 123 fuck","hellofuck");
+	$feedback=str_replace($offcolor, '%!@*', $feedback);
+	echo "<p>$feedback[1]</p>";
+
+
+
+
+	
 ?>
 <html>
 	<head>
