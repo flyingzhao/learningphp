cecae326951a1b05f32865af58499ccc51dcdc3e
zhao <1142749677@qq.com>
2015-07-27 19:49:05 +0800

    ch27

--- ch27/add_bms.php
+++ ch27/add_bms.php
@@ -1,6 +1,33 @@
 <?php
 require_once('bookmark_fns.php');
 session_start();
+$new_url=$_POST['new_url'];
+do_html_header('Adding bookmarks');
+try {
+	check_valid_user();
+	if (!filled_out($POST)) {
+		throw new Exception("Form not completely filled out");	
+	} 
 
+	if (strstr($new_url, 'http://')===false) {
+		$new_url="http://".$new_url;
+	} 
+
+	if (!(@fopen($new_url, 'r'))) {
+		throw new Exception("Not valid URL");
+	}
+
+	add_bm($new_url);
+	echo "Bookmark added";
+	
+	if ($url_array=get_user_urls($_SESSION['valid_user'])) {
+		display_user_urls($url_array);
+	}
+
+} catch (Exception $e) {
+	echo $e->getMessage();	
+}
+display_user_menu();
+do_html_footer();
 
 ?>
--- ch27/url_fns.php
+++ ch27/url_fns.php
@@ -1,5 +1,36 @@
 <?php
-// function get_user_urls(){
+
+function add_bm($new_url){
+	echo "Attempting to add ".htmlspecialchars($new_url)."<br />";
+	$conn=db_connect();
+	// check not a repeat bookmark
+  	$result = $conn->query("select * from bookmark
+                         		where username='".$_SESSION['valid_user']."
+                         		and bm_URL='".$new_url."'");
+ 	if ($result && ($result->num_rows>0)) {
+    		throw new Exception('Bookmark already exists.');
+  	}
 	
-// }
+	$result=$conn->query("insert into bookmark values('".$_SESSION['valid_user']."','".$new_url."')");
+	if (!$result) {
+		throw new Exception("Can not query", 1);
+		
+	}
+	return true;
+}
+function get_user_urls($username){
+	//extract from the database all the URLs
+	$conn=db_connect();
+	$result=$conn->query("select bm_url from bookmark where username='".$username."'");
+	if (!$result) {
+		return false;
+	}
+	$url_array=array();
+	for ($i=0;$row=$result->fetch_row(); $i++) { 
+		$url_array[$i]=$row[0];
+	}
+	return $url_array;
+		
+}
+
 ?>
